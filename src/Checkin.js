bb7b4fc9ba292779b7c27619029e54585173a07c
Justin Hamerly <suvimountain@icloud.com>
2021-09-06 11:18:39 -0700

    added header and routes

---
+++ src/Checkin.js
@@ -0,0 +1,11 @@
+import {Component} from 'react';
+
+class Checkin extends Component {
+  render(){
+    return{
+
+    };
+  }
+}
+
+export default Checkin;
---
+++ src/MyMap.js
@@ -0,0 +1,11 @@
+import {Component} from 'react';
+
+class MyMap extends Component {
+  render(){
+    return{
+
+    };
+  }
+}
+
+export default MyMap;
