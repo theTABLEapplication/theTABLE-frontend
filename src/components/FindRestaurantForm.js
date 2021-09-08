722f0a4e6c141e4b04c9b6747c783ebdea55cee3
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-09-07 23:40:21 -0700

    updated add a restaurant modal

--- src/components/FindRestaurantForm.js
+++ src/components/FindRestaurantForm.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import '../css/FindRestForm.css';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 class FindRestaurantForm extends Component {
   handleSubmit = event => {
@@ -18,15 +17,13 @@ class FindRestaurantForm extends Component {
   render() {
     return (
       <Form onSubmit={this.handleSubmit}>
-        <Form.Group controlId='restaurantName' id="formGroup">
-          <FloatingLabel label="Enter Restaurant Name">
-            <Form.Control type='name' placeholder='Enter Restaurant Name Here' />
-          </FloatingLabel>
+        <Form.Group controlId='restaurantName'>
+          <Form.Label>Restaurant Name</Form.Label>
+          <Form.Control type='name' placeholder='Enter Restaurant Name Here' />
         </Form.Group>
         <Form.Group controlId='restaurantLocation'>
-          <FloatingLabel label="Enter Restaurant City, Zip Code, or Address Here">
-            <Form.Control type='location' placeholder='Enter City, Zip Code, or Address' />
-          </FloatingLabel>
+          <Form.Label>Restaurant Location</Form.Label>
+          <Form.Control type='location' placeholder='Enter City, Zip Code, or Address Here' />
         </Form.Group>
         <Button id="findRestButton" type='submit'>
           Submit
