39435abe755a5ef4181bb52c93a0d49135bf174a
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-09-09 20:20:23 -0700

    fixed profile nav link to render conditionally

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ class Header extends Component {
       <Navbar collapseOnSelect expand="lg" id="navBar">
         <Navbar.Brand id="navBarTitle"><img src={logo} /> theTABLE </Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-        <NavItem><Link to="/profile" className="nav-link">Profile</Link></NavItem>
+        {this.props.auth0.isAuthenticated ? (<NavItem><Link to="/profile" className="nav-link">Profile</Link></NavItem>) : null}
         <NavItem><Link to="/aboutus" className="nav-link">About Us</Link></NavItem>
         {this.props.auth0.isAuthenticated ? <Logout /> : <Login />}
       </Navbar>
